a9e804a85bb778c911fcbc3cd2c31247fce1ff82
zxfhuy <51502210+zxfhuy@users.noreply.github.com>
2024-03-12 21:32:03 +0800

    Create cctv.php

---
+++ cctvpy/cctv.php
@@ -0,0 +1,374 @@
+{
+	 "list" : [{
+  "vod_id": "TOPC1451528971114112",
+  "vod_name": "新闻联播",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451558976694518",
+  "vod_name": "焦点访谈",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451464665008914",
+  "vod_name": "今日说法",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451378757637200",
+  "vod_name": "等着我",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451559129520755",
+  "vod_name": "新闻直播间",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451540328102649",
+  "vod_name": "海峡两岸",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451530382483536",
+  "vod_name": "天网",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451540389082713",
+  "vod_name": "今日关注",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451557893544236",
+  "vod_name": "探索·发现",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1665739007799851",
+  "vod_name": "高端访谈",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451557052519584",
+  "vod_name": "百家讲坛",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451464884159276",
+  "vod_name": "开讲啦",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451464884159276",
+  "vod_name": "故事里的中国",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1514182710380601",
+  "vod_name": "对话",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451559038345600",
+  "vod_name": "面对面",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451378967257534",
+  "vod_name": "动物世界",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451558190239536",
+  "vod_name": "走进科学",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451525103989666",
+  "vod_name": "人与自然",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451557421544786",
+  "vod_name": "地理·中国",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451541349400938",
+  "vod_name": "远方的家",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451558150787467",
+  "vod_name": "自然传奇",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451534366388377",
+  "vod_name": "是真的吗",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451467630488780",
+  "vod_name": "星光大道",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451557646802924",
+  "vod_name": "健康之路",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451559025546574",
+  "vod_name": "动画大放映",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451378857272262",
+  "vod_name": "第一动画乐园",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451541414450906",
+  "vod_name": "精彩音乐汇",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451534421925242",
+  "vod_name": "音乐厅",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451541994820527",
+  "vod_name": "民歌·中国",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1575253587571324",
+  "vod_name": "跟着书本去旅行",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451354597100320",
+  "vod_name": "中国电影报道",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451469943519994",
+  "vod_name": "星推荐",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1571217727564820",
+  "vod_name": "方圆剧阵",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451558856402351",
+  "vod_name": "空中剧院",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1650782829200997",
+  "vod_name": "正大综艺",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451550970356385",
+  "vod_name": "体坛快讯",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451530259915198",
+  "vod_name": "第一时间",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451465894294259",
+  "vod_name": "开门大吉",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451464884159276",
+  "vod_name": "开讲啦",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451558858788377",
+  "vod_name": "共同关注",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451527941788652",
+  "vod_name": "军事报道",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451558819463311",
+  "vod_name": "新闻调查",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451559097947700",
+  "vod_name": "新闻30分",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451559066181661",
+  "vod_name": "新闻1+1",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451540448405749",
+  "vod_name": "今日亚洲",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451559129520755",
+  "vod_name": "新闻直播间",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451558428005729",
+  "vod_name": "24小时",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451539894330405",
+  "vod_name": "中国新闻",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451558779639282",
+  "vod_name": "午夜新闻",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451558496100826",
+  "vod_name": "朝闻天下",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451528792881669",
+  "vod_name": "晚间新闻",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451559180488841",
+  "vod_name": "新闻周刊",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1601362002656197",
+  "vod_name": "经济半小时",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451533652476962",
+  "vod_name": "经济大讲堂",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1453100395512779",
+  "vod_name": "正点财经",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451546588784893",
+  "vod_name": "生活圈",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451526037568184",
+  "vod_name": "生活提示",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451558532019883",
+  "vod_name": "东方时空",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451533782742171",
+  "vod_name": "经济信息联播",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1571034705435323",
+  "vod_name": "今日环球",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+},
+{
+  "vod_id": "TOPC1451543462858283",
+  "vod_name": "一线",
+  "vod_remarks": "CNTV",
+  "vod_pic": "https://wx1.sinaimg.cn/mw690/b06dbec0ly1hnkco6g1y0j20go0gotao.jpg@User-Agent=Dart/2.19 (dart:io)"
+}],
+	 
+	"code": 1,
+	"msg": 200,
+	"class": [{
+    "type_name": "央视精选",
+    "type_id": "1"
+}]}
