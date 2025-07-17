37798468b9b7295a7a90b4bdaefbb37a4a8a8d86
zxfhuy <51502210+zxfhuy@users.noreply.github.com>
2025-07-17 15:50:30 +0800

    Add files via upload

--- js/短剧大全.js
+++ js/短剧大全.js
@@ -1,11 +1,10 @@
 var rule = {
     类型: '影视', //影视|听书|漫画|小说
-    title: '爱看短剧[盘]',
-    host: 'https://ys.110t.cn/',
-    homeUrl: '/api/ajax.php?act=recommend',
-    homeUrl: '/api/ajax.php?act=Daily',
-    url: '/api/ajax.php?act=fyclass',
-    searchUrl: '/api/ajax.php?act=search&name=**',
+    title: '短剧',
+    host: 'https://ai-img.ycubbs.cn/',
+    homeUrl: '/api/duanju/list',
+    url: '/api/duanju/list',
+    searchUrl: 'https://api.kuleu.com/api/action?text=**',
     searchable: 1,
     quickSearch: 0,
     filterable: 0,
@@ -15,15 +14,15 @@ var rule = {
     hikerListCol: "text_1",
     hikerClassListCol: "text_1",
     timeout: 5000,
-    class_name: '全部',
-    class_url: 'yingshilist',
+    class_name: '',
+    class_url: '',
     play_parse: true,
     lazy: $js.toString(() => {
         input = "push://" + input;
     }),
     double: false,
     推荐: '*',
-    一级: 'json:data;name;;addtime;url',
+    一级: 'json:data;name;;;url',
     二级: '*',
-    搜索: '*',
+    搜索: 'json:data;name;;;viewlink',
 }
